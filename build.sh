30ab2f87080859b9ff648033c1f968790b2bf55f
jorge <jorge@aplisein.com>
2023-03-09 10:04:34 +0100

    options

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo "Autobuild v0.0.1"
+echo "Made by: jorgeju4rez"
+echo "Licence: MIT"
+echo \	
+echo "==== Menu options ===="
+
+
+echo "1. Docker"
+echo "2. Kubernetes"
+echo "3. Licence"
+echo "4. Exit"
+echo "======================"
+echo \	
+
