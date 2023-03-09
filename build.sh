10716a28c3e4bb367945d3f77bf2ec9efcb29976
jorge <jorge@aplisein.com>
2023-03-09 10:08:22 +0100

    new changes

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ echo "Made by: jorgeju4rez"
 echo "Licence: MIT"
 echo \	
 echo "==== Menu options ===="
-
-
 echo "1. Docker"
 echo "2. Kubernetes"
 echo "3. Licence"
@@ -15,3 +13,22 @@ echo "4. Exit"
 echo "======================"
 echo \	
 
+read -p 'Select menu option: ' userOption
+
+case $userOption in
+1)
+   echo "Docker"
+   ;;
+2)
+   echo "Kubernetes"
+   ;;
+3) 
+   echo "Licence"
+   ;;
+4)
+   echo "Exit"
+   ;;
+*)
+   echo "default"
+   ;;
+esac
