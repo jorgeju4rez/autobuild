fcec2ec2c16c0aa551f72c1c89f67f8cd0d321e0
jorge <jorge@aplisein.com>
2023-03-09 10:20:23 +0100

    test

--- build.sh
+++ build.sh
@@ -15,20 +15,59 @@ echo \
 
 read -p 'Select menu option: ' userOption
 
+
+
+fun_docker () {
+   mkdir deploy
+   cd deploy
+   touch Dockerfile docker-compose.yml docker-compose.env
+   printf "version: "3.0"
+
+services:
+  name-image:
+    container_name: name-image
+    build:
+      context: ../../
+      dockerfile: deploy/docker/Dockerfile
+    restart: unless-stopped
+#    ports:
+#      - "3000:80"
+    image: name-image
+" >> docker-compose.yml
+}
+
+fun_kubernetes () {
+   echo "Kubernetes";
+}
+
+fun_licence () {
+   clear
+   echo "MIT License
+Copyright (c) 2023 jorgeju4rez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+}
+
 case $userOption in
 1)
-   echo "Docker"
+   fun_docker
    ;;
 2)
-   echo "Kubernetes"
+   fun_kubernetes
    ;;
 3) 
-   echo "Licence"
+   fun_licence
    ;;
 4)
-   echo "Exit"
+   exit
    ;;
 *)
    echo "default"
    ;;
 esac
+
+
